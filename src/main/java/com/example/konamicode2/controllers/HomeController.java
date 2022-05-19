db4d1662831728e6b013271fcb91ff8404928f11
Samuel Arguello <sam.rene12@gmail.com>
2022-05-19 14:29:31 -0500

    began work on login page and register logic

--- src/main/java/com/example/konamicode2/controllers/HomeController.java
+++ src/main/java/com/example/konamicode2/controllers/HomeController.java
@@ -2,10 +2,25 @@ package com.example.konamicode2.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@Controller
 public class HomeController {
-    @Controller
-    @RequestMapping
+
+    // maps to on load page where user can register for an account or login existing
+    @RequestMapping("/")
+    public String login(){
+        return "game/userlogin";
+    }
+
+
+
+
+
+
+
+
+
+    // maps to the home page upon user logging in or registering
+    @RequestMapping("/home")
     public String home(){
         return "game/home";
     }
